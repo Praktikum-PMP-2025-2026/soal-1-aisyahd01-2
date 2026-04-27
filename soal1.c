cd993aee8c83cecb791f12b34842c8abeb54c7a0
aisyahd01 <aisyahdahayu02@gmail.com>
2026-04-27 08:59:37 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,42 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void sort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j+1]) {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+void median (int arr[], int n){
+    if (n%2==1){
+        printf("MEDIAN %d\n", arr[n/2]);}
+    else { 
+        int a=arr[n/2-1];
+        int b=arr[n/2];
+        float m=(a+b)/2.0;
+        printf("MEDIAN %.2f\n", m);
+
+    }
+
+}
+
+void urut (int arr[], int n){
+    for (int i=0; i<n-1; i++){
+        for (int j=0; j<n-i-1;j++){
+            if (arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr [j]=arr[j+1];
+                arr [j+1]=temp;
             }
+
+
         }
     }
 }
 
-void median(int arr[], int n) {
-    printf("MEDIAN ");
-    if (n % 2 == 1) {
-        printf("%d\n", arr[n/2]);
-    } else {
-        int a = arr[n/2 - 1];
-        int b = arr[n/2];
-        float m = (a + b) / 2.0;
-
-        if ((a + b) % 2 == 0)
-            printf("%.0f\n", m);
-        else
-            printf("%.2f\n", m);
-    }
-}
-
-int main() {
+int main (){
     int *arr;
-    int size = 0, capacity = 2;
+    int size=0; 
+    int capacity=2;
 
-    arr = (int*) malloc(capacity * sizeof(int));
+    arr=(int*)malloc(capacity*sizeof(int));
 
     int x;
-    while (scanf("%d", &x) && x != -1) {
+    while (scanf("%d", &x) && x!=-1) {
         if (size == capacity) {
             capacity *= 2;
             arr = (int*) realloc(arr, capacity * sizeof(int));
@@ -44,23 +44,28 @@ int main() {
         arr[size++] = x;
     }
 
-    sort(arr, size);
-
-    printf("COUNT %d\n", size);
-
-    printf("SORTED ");
+   printf("SORTED ");
     for (int i = 0; i < size; i++) {
-        printf("%d", arr[i]);
-        if (i < size - 1) printf(" ");
-    }
+        printf("%d", arr[i]);}
     printf("\n");
 
-    median(arr, size);
+    urut (arr, size);
+    printf ("COUNT %d", size);
+    median (arr, size); 
+    free (arr);
+return 0;
+}
 
-    free(arr);
-    return 0;
-}
-}
+
+
+
+    
+
+
+
+
+
+
 
 
     
