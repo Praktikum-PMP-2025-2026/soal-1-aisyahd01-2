10d293e3e333e1f8085d20d285b2872227382ad6
aisyahd01 <aisyahdahayu02@gmail.com>
2026-04-27 08:55:53 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,39 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void median (int arr[], int n){
-    if (n%2==1){
-        printf("MEDIAN %d\n", arr[n/2]);}
-    else { 
-        int a=arr[n/2-1];
-        int b=arr[n/2];
-        float m=(a+b)/2.0;
-        printf("MEDIAN %.2f\n", m);
-
-    }
-
-}
-
-void urut (int arr[], int n){
-    for (int i=0; i<n-1; i++){
-        for (int j=0; j<n-i-1;j++){
-            if (arr[j]>arr[j+1]){
-                int temp=arr[j];
-                arr [j]=arr[j+1];
-                arr [j+1]=temp;
+void sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
-
-
         }
     }
 }
 
-int main (){
-    int *arr;
-    int size=0; 
-    int capacity=2;
+void median(int arr[], int n) {
+    printf("MEDIAN ");
+    if (n % 2 == 1) {
+        printf("%d\n", arr[n/2]);
+    } else {
+        int a = arr[n/2 - 1];
+        int b = arr[n/2];
+        float m = (a + b) / 2.0;
 
-    arr=(int*)malloc(capacity*sizeof(int));
+        if ((a + b) % 2 == 0)
+            printf("%.0f\n", m);
+        else
+            printf("%.2f\n", m);
+    }
+}
+
+int main() {
+    int *arr;
+    int size = 0, capacity = 2;
+
+    arr = (int*) malloc(capacity * sizeof(int));
 
     int x;
     while (scanf("%d", &x) && x != -1) {
@@ -44,18 +44,22 @@ int main (){
         arr[size++] = x;
     }
 
-   printf("SORTED ");
+    sort(arr, size);
+
+    printf("COUNT %d\n", size);
+
+    printf("SORTED ");
     for (int i = 0; i < size; i++) {
         printf("%d", arr[i]);
         if (i < size - 1) printf(" ");
     }
     printf("\n");
-    
-    urut (arr, size);
-    printf ("COUNT %d", size);
-    median (arr, size); 
-    free (arr);
+
+    median(arr, size);
+
+    free(arr);
     return 0;
+}
 }
 
 
